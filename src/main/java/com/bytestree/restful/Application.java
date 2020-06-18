d728bf03308b2fa8141ed5906c65244416f81aa8
root <root@ip-172-31-63-237.ec2.internal>
2020-06-18 19:37:59 +0000

    jar to war by boot

---
+++ src/main/java/com/bytestree/restful/Application.java
@@ -0,0 +1,24 @@
+package com.bytestree.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+/**
+ * 
+ * @author hbhosale
+ *
+ */
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+        @Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder app) { 
+	  return app.sources(Application.class); 
+	}	
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+ }	
---
+++ src/main/java/com/bytestree/restful/service/CRUDService.java
@@ -0,0 +1,18 @@
+package com.bytestree.restful.service;
+
+/**
+ * @author hbhosale
+ */
+import java.io.Serializable;
+import java.util.List;
+
+public interface CRUDService<E> {
+
+	E save(E entity);
+
+	E getById(Serializable id);
+
+	List<E> getAll();
+
+	void delete(Serializable id);
+}
